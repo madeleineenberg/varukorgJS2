137f24211d4ea72c52b7da85b75d10cbe86258f6
madeleineenberg <madeleineenberg@Apples-MacBook-Air.local>
2020-02-06 15:01:05 +0100

    list update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     let data = user.products;
     $.each(data, function(i, name) {
       $("#product-container").append(
-        '<div class="card" style="width: 18rem;"' +
+        "<div class='product-card'>" +
           "<div>" +
           "<img class='card-img-top'src=" +
           data[i].img +
@@ -16,6 +16,10 @@ $(document).ready(function() {
           data[i].price +
           "</span>" +
           "<span>kr</span>" +
+          "<br>" +
+          '<input type="number" max="10" min="1" value="1" class="product-qty" />' +
+          "<br>" +
+          "<br>" +
           '<button class="btn btn-warning">Köp</button>' +
           "</div>" +
           "</div>"
@@ -28,34 +32,49 @@ $(document).ready(function() {
       event.preventDefault();
       let prod = $(this).parent();
       let litext = prod.find(".product");
-      let liPrice = prod.find(".price");
+      let listPrice = prod.find(".price");
+      let productQty = prod.find("input").val();
+
       $("#cart-list").append(
         "<li class='cart-row'>" +
           litext.text() +
           " " +
           "<span class='product-price'>" +
-          liPrice.text() +
+          listPrice.text() +
           "</span>" +
           "<span>kr</span>" +
           " " +
+          '<input type="number" max="10" min="1" value="' +
+          productQty +
+          '" class="input-qty" />' +
           "<button class='remove-btn'>Ta bort</button>" +
           "</li>"
       );
 
+      $(".product-qty").val("1");
       $(".remove-btn").click(removeCartItem);
-
       getTotalCost();
+      $(".input-qty").on("input", getTotalCost);
     }
 
     function getTotalCost() {
       let totalPrice = 0;
-      $(".cart-row")
-        .find(".product-price")
-        .each(function() {
-          totalPrice += parseFloat($(this).html());
-        });
+      let $cartRow = $(".cart-row");
+      //loopar över varje rad i varukorgen
+      $($cartRow).each(function() {
+        //hittar priset och antalet på this.cartrow för varje loop
+        let $prodprice = $(this).find(".product-price");
+        let $prodqty = $(this)
+          .find("input")
+          .val();
+        // gör om från strängar till floats med parseFloat
+        let $pPrice = parseFloat($prodprice.html());
+        let $pQty = parseFloat($prodqty);
+        totalPrice += $pPrice * $pQty;
+      });
+      //lägger till den nya totalen på sidan
       document.getElementById("total").innerHTML =
-        "<strong>SUMMA:</strong>" + totalPrice + "kr" + "<br>";
+        "<strong>SUMMA:</strong>" + totalPrice + "<br>";
     }
 
     $("#order").click(function() {
@@ -69,5 +88,10 @@ $(document).ready(function() {
       buttonClicked.parentElement.remove();
       getTotalCost();
     }
+
+    $("#remove-all").click(function() {
+      $("ul").empty();
+      getTotalCost();
+    });
   });
 });
